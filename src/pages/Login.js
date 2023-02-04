231e7c051b2266bb0d8b1f3f8e054250cae7a9f1
andreresende36 <andreresende36@gmail.com>
2023-02-04 14:18:51 -0300

    Feature 1: done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { saveUser } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    disabled: true,
+  };
+
+  checkButton = () => {
+    const { email, password } = this.state;
+    const six = 6;
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const validation1 = emailRegex.test(email);
+    const validation2 = password.length >= six;
+    this.setState({ disabled: !(validation1 && validation2) });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.checkButton);
+  };
+
+  handleClick = () => {
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(saveUser(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabled } = this.state;
+    const { handleChange, handleClick } = this;
+    return (
+      <>
+        <input
+          type="email"
+          name="email"
+          data-testid="email-input"
+          placeholder="Digite aqui seu e-mail"
+          onChange={ handleChange }
+          value={ email }
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          placeholder="Senha com no mínimo 6 dígitos"
+          onChange={ handleChange }
+          value={ password }
+        />
+        <button
+          type="button"
+          disabled={ disabled }
+          onClick={ handleClick }
+        >
+          Entrar
+        </button>
+      </>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+import { SAVE_USER } from './actionTypes';
+
+const saveUser = (email) => ({
+  type: SAVE_USER,
+  payload: email,
+});
+
+export { saveUser, SAVE_USER };
